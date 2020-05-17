5b9d4747ee742de35257c4835bf65b976253159e
Nishant Singh <nishantsingh9527@gmail.com>
2020-05-17 22:13:17 +0530

    Main Code for sending the mail in both the condition

---
+++ sendmail.py
@@ -0,0 +1,16 @@
+import smtplib
+
+sender_email = "nishantsingh70600@gmail.com"
+receiver_email = "nishantsingh9527@gmail.com"
+password = input(str("Please enter the password"))
+message = "your code is not correct."
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+
+server.login(sender_email,password)
+print('login successfully')
+
+server.sendmail(sender_email, receiver_email , message)
+print("Email has been sent to ", receiver_email)
+server.quit()
